21b3a1b10de73c66b1a6d8554eff4e76cb01e410
pacscs <74614731+pacscs@users.noreply.github.com>
2021-01-08 09:49:41 -0300

    Initial Commit

---
+++ Gas Guzzler/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Gas Guzzler
+//
+//  Created by Paulo Alfredo Coraini de Souza on 08/01/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
